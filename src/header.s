5c238151de23776e20bf3f9a8e5aa76b40697016
MrTipson <gunfight7.gf@gmail.com>
2022-07-08 23:20:37 +0200

    Code refactor, add skippable characters and comments

---
+++ src/header.s
@@ -0,0 +1,11 @@
+@ Define my Raspberry Pi
+.cpu    cortex-a7
+.fpu    neon-vfpv4
+.syntax unified         @ modern syntax
+
+@ Program code
+.text
+.align  2
+.global _start
+
+.include "src/stdlib.s"
---
+++ src/stdlib.s
@@ -0,0 +1,26 @@
+exit:
+	mov	r7,#1		@ syscall code
+	svc	0
+	bx	lr
+
+getchar:
+	push	{r1,r2,r7,lr}
+	mov	r7,#3		@ read syscall code
+	mov	r0,#0		@ fd 0
+	ldr	r1,=cbuf	@ char buffer
+	mov	r2,#1		@ count
+	strb	r0,[r1]
+	svc	0
+	ldrb	r0,[r1]		@ load read character
+	pop	{r1,r2,r7,lr}
+	bx	lr
+
+putchar:
+	push	{r1,r2,r7,lr}
+	mov	r7,#4		@ write syscall code
+	mov	r0,#1		@ fd 1
+	ldr	r1,=cbuf	@ char buffer
+	mov	r2,#1		@ count
+	svc	0
+	pop	{r1,r2,r7,lr}
+	bx	lr
